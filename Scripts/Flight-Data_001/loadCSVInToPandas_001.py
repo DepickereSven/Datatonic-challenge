32aaf5c76ef089c88fd4163806c1af815f200f51
svend <sven.depickere@student.howest.be>
2018-04-21 11:12:11 +0200

    Fixed an error in the path added some more options

--- Scripts/Flight-Data_001/loadCSVInToPandas_001.py
+++ Scripts/Flight-Data_001/loadCSVInToPandas_001.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('../Initial_Data/Unzipped/flights_2010_000000000000/flights_2010_000000000001')
+df = pd.read_csv('../../Initial_Data/Unzipped/flights_2010_000000000001/flights_2010_000000000001', index_col = 0)
 print(df)
+df.info()
+print(df.describe())
--- Scripts/Flight-Data_002/loadCSVInToPandas_002.py
+++ Scripts/Flight-Data_002/loadCSVInToPandas_002.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('../Initial_Data/Unzipped/flights_2010_000000000000/flights_2010_000000000002')
+df = pd.read_csv('../../Initial_Data/Unzipped/flights_2010_000000000002/flights_2010_000000000002', index_col = 0)
 print(df)
+df.info()
+print(df.describe())
